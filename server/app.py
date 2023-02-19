861b91dc3a5abc86285564743faf773b95fcf5ef
Aidan <27599708+Adawg4@users.noreply.github.com>
2023-02-19 00:35:39 -0800

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,9 +3,10 @@ from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from mangum import Mangum
-import asyncio, json, uuid, boto3
+import asyncio, json, uuid, boto3, aiohttp
 from botocore.exceptions import ClientError
 import requests
+
 
 
 app = FastAPI()
@@ -52,65 +53,6 @@ async def search(request: Request, query: str = Form(...)):
 '''
 #https://fastapi.tiangolo.com/advanced/custom-response/#streamingresponse
 
-@app.websocket("/audio")
-async def audio(websocket: WebSocket):
-    try:
-        # Create an Amazon Transcribe streaming client
-        client = boto3.client('transcribe', region_name='us-east-1')
-        language_code = "en-US"
-
-        # Generate a unique identifier for this transcription job
-        job_name = str(uuid.uuid4())
-
-        # Start the transcription job
-        response = client.start_stream_transcription(
-            LanguageCode=language_code,
-            MediaSampleRateHertz=16000,
-            MediaEncoding='pcm',
-            AudioStream=websocket
-        )
-
-        # Initialize variables for speaker detection algorithm
-        transcript = ""
-        last_event_time = 0
-        pause_threshold = 1.5  # Pause duration threshold in seconds
-
-        # Keep streaming the audio data to Transcribe and handle the results
-        async for message in websocket.iter_text():
-            response = json.loads(message)
-            results = response.get("Transcript", {}).get("Results", [])
-
-            for result in results:
-                if result.get("Alternatives"):
-                    new_transcript = result["Alternatives"][0]["Transcript"]
-                    confidence = result["Alternatives"][0]["Confidence"]
-                    transcript += new_transcript
-
-                    # Check if the speaker has stopped speaking
-                    time_diff = result["ResultEndTime"] - last_event_time
-                    if time_diff > pause_threshold:
-                        # Send the transcript to the client
-                        await websocket.send_text(f"Transcription: {transcript} (Confidence: {confidence})")
-
-                        # Send the transcript to https://modal.com/ using an API request
-                        response = requests.post("https://modal.com/api/transcripts", json={"transcript": transcript})
-                        if response.status_code == 200:
-                            await websocket.send_text(f"Transcript sent to https://modal.com/")
-                        else:
-                            await websocket.send_text(f"Failed to send transcript to https://modal.com/")
-
-                        # Reset variables for the next speaker
-                        transcript = ""
-                    last_event_time = result["ResultEndTime"]
-
-    except ClientError as e:
-        await websocket.send_text(f"Error: {e}")
-    except Exception as e:
-        await websocket.send_text(f"Error: {e}")
- 
-
-
-app = FastAPI()
 
 async def transcribe_audio(language_code: str, websocket: WebSocket):
     try:
@@ -147,6 +89,13 @@ async def transcribe_audio(language_code: str, websocket: WebSocket):
                     # Check if the speaker has stopped speaking
                     time_diff = result["ResultEndTime"] - last_event_time
                     if time_diff > pause_threshold:
+                        # Send the transcript to the external API
+                        async with aiohttp.ClientSession() as session:
+                            url = "https://adityarai10101--vectordbqaadi-queryreal.modal.run/?x=how+many+products+do+you+sell"
+                            payload = {"transcription": transcript, "confidence": confidence}
+                            async with session.post(url, json=payload) as response:
+                                pass
+
                         # Yield the transcript to the client
                         yield f"Transcription: {transcript} (Confidence: {confidence})\n"
 
